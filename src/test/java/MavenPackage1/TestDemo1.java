7ece9d6f0b42847930cda49d550d60be501ea0ab
Annapurna1995 <52752609+Annapurna1995@users.noreply.github.com>
2019-08-27 16:26:13 +0530

    FIRST MAVEN COMMIT BY SRINIDHI 27/08/2019

---
+++ src/test/java/MavenPackage1/TestDemo1.java
@@ -0,0 +1,29 @@
+package MavenPackage1;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+
+public class TestDemo1
+{
+	static
+	{
+		System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
+		
+	}
+
+	@Test
+	public void test1()
+	{
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("http://www.google.com");
+		System.out.println(driver.getTitle());
+		driver.close();
+	}
+}
